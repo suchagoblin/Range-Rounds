9b28c9042f8927dec3f8799f310ada79037bd21f
suchagoblin <jarrod.l.farmer@gmail.com>
2026-01-31 22:38:25 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // This tells the app to look for files in the current folder
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
